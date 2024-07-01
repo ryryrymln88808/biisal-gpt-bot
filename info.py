b67a50a7b2cd5098be4ec7efbb62bee41ae0a23a
ryryrymln88808 <ryryrymln88808@gmail.com>
2024-07-01 11:15:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,17 +1,17 @@
 # ©️biisal jai shree krishna 😎
 from os import environ
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_NAME = environ.get("BOT_NAME", "BISAL CHAT BOT")
-ADMIN = int(environ.get("ADMIN", ""))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1001812797837"))
-ADMIN_NAME = environ.get("ADMIN_NAME", "Bisal")
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "")
-MONGO_URL = environ.get("MONGO_URL", "")
+API_ID = environ.get("API_ID", "25848289")
+API_HASH = environ.get("API_HASH", "88cf2c17c7a2bd21f4204c89c648dd40")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7478651130:AAHZConluCXIQkBOBytbk_Xl9BELQc7PCcE")
+BOT_NAME = environ.get("BOT_NAME", "RAS CHAT BOT")
+ADMIN = int(environ.get("ADMIN", "1557042262"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002124706394"))
+ADMIN_NAME = environ.get("ADMIN_NAME", "Anonymous_7")
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002246762613")
+MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://ryryrymln88808:6CrFpGCUpkRvjBbZ@cluster0.j2xo87f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-100XXXXXXXXXX")
+    environ.get("AUTH_CHANNEL", "-1001923834324")
 )  # add your channel id for force subscribe
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
